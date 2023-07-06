87edbb9446b8e30707ceecac2797d3b3afc9d868
Vania <vnmrnda@gmail.com>
2023-07-06 16:16:41 +0700

    loginlogout

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,43 @@
 const { User, Profile, TravelAgent, Itinerary, Schedule } = require('../models')
 
 class Controller {
+    static login(req, res) {
+        res.render('login', { message: req.session.message });
+      }
+
+    static getLogin(req, res) {
+    const { email, password } = req.body;
+
+    if (email && password) {
+        User.findAll({ where: { email } })
+        .then((users) => {
+            if (users.length > 0 && users[0].password === password) {
+            req.session.user = {
+                email: users[0].email
+            };
+            res.redirect('/');
+            } else {
+            req.session.message = 'Username or password is incorrect';
+            res.redirect('/login');
+            }
+        })
+        .catch((err) => {
+            req.session.message = 'An error occurred';
+            res.redirect('/login');
+        });
+    } else {
+        req.session.message = 'Username or password is incorrect';
+        res.redirect('/login');
+    }
+    }
+
+    static logout(req, res) {
+        req.session.destroy(() => {
+          res.redirect('/login');
+        });
+      }
+
+    
     static findSchedule(req, res){
         Schedule.findAll({
             include: TravelAgent, Itinerary
@@ -15,5 +52,7 @@ class Controller {
             res.send(err)
         })
     }
+
+
 }
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const Controller = require('../controllers/controller')
 
 router.get('/', Controller.findSchedule)
+router.get('/login',Controller.login )
+router.post('/login',Controller.getLogin )
+router.post('/logout', Controller.logout);
+
+
 
 module.exports = router;
