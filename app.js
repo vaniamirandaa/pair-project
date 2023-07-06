3c02b977ecf3a1689a1648374c4f03d9c01547c0
Vania <vnmrnda@gmail.com>
2023-07-06 11:09:42 +0700

    package

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const session = require('express-session');
 const router = require('./routes/index');
 const app = express();
 const port = 3000;
+const easyinvoice = require('easyinvoice');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const password = "Admin@123"
+
+
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +20,25 @@ app.use(session({
 }));
 
 app.use(router);
+
+
+bcrypt
+.hash(password, saltRounds)
+.then(hash => {
+  userHash = hash 
+  console.log('Hash ', hash)
+  validateUser(hash)
+})
+.catch(err => console.error(err.message))
+
+function validateUser(hash) {
+  bcrypt
+  .compare(password, hash)
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => console.error(err.message))        
+}
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const easyinvoice = require('easyinvoice');
 
+
+
+router.get('/invoice', (req, res) => {
+    const invoiceData = {
+      currency: 'USD',
+      taxNotation: 'tax',
+      marginTop: 25,
+      marginRight: 25,
+      marginLeft: 25,
+      marginBottom: 25,
+      logo: 'path/to/your/logo.png',
+      sender: {
+        company: 'Your Company',
+        address: '123 Street, City, Country',
+      },
+      client: {
+        company: 'Client Company',
+        address: '456 Street, City, Country',
+        phone: '+9876543210',
+        email: 'client@example.com',
+      },
+      invoiceNumber: 'INV-001',
+      invoiceDate: '2023-07-06',
+      products: [
+        {
+          quantity: 2,
+          description: 'Product 1',
+          tax: 10,
+          price: 50,
+        },
+        {
+          quantity: 1,
+          description: 'Product 2',
+          tax: 10,
+          price: 100,
+        },
+      ],
+      bottomNotice: 'Thank you for your business!',
+    };
+  
+    const invoiceOptions = {
+      filename: 'invoice.pdf',
+    };
+  
+    easyinvoice.createInvoice(invoiceData, invoiceOptions, (result, err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('An error occurred');
+      } else {
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=invoice.pdf');
+        res.send(result.pdf);
+      }
+    });
+  });
+  
 module.exports = router
